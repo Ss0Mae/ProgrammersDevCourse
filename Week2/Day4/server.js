8c093c48dc33515f6daff5254454d58b500019b3
Ss0Mae <ssomae@protonmail.ch>
2024-08-22 17:22:20 +0900

    module

--- Week2/Day4/server.js
+++ Week2/Day4/server.js
@@ -1,8 +1,12 @@
-let http = require('http');
+let http = require('http'); // 사용할 프로토콜 설정
+function start() {
 
-function onRequest(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write('Hello Node.js');
-    response.end();
+    function onRequest(request, response) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write('Hello Node.js');
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
 }
-http.createServer(onRequest).listen(8888);
+
+exports.start = start // 밖에서도 사용가능하게 해준다
