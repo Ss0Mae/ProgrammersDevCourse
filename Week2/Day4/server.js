033dcb9d22d295be9955e25cb4d41c0cc9107bb2
Ss0Mae <ssomae@protonmail.ch>
2024-08-22 16:32:54 +0900

    NodeJS

---
+++ Week2/Day4/server.js
@@ -0,0 +1,8 @@
+let http = require('http');
+
+function onRequest(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('Hello Node.js');
+    response.end();
+}
+http.createServer(onRequest).listen(8888);
