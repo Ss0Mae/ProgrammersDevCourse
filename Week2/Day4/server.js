75c64bcf0cfb3295a60f009c652e107a197e839e
Ss0Mae <ssomae@protonmail.ch>
2024-08-22 17:32:34 +0900

    router

--- Week2/Day4/server.js
+++ Week2/Day4/server.js
@@ -1,7 +1,12 @@
 let http = require('http'); // 사용할 프로토콜 설정
-function start() {
+let url = require('url');
 
+function start(route) {
+    
     function onRequest(request, response) {
+        let pathname = url.parse(request.url).pathname;
+        route(pathname);
+
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.write('Hello Node.js');
         response.end();
