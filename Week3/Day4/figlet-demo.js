45e6ce74ac49fbb948f839e57627f1c9054462d3
Ss0Mae <ssomae@protonmail.ch>
2024-08-29 17:56:21 +0900

    CallBack Function

--- Week3/Day4/figlet-demo.js
+++ Week3/Day4/figlet-demo.js
@@ -1,6 +1,7 @@
 var figlet = require("figlet");
 
 figlet("SeongMin!!", function (err, data) {
+    //익명의 함수를 쓰는 이유 = 이 함수를 쓸 일이 다른 데는 없어서
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
--- Week3/Day4/nonBlockingDemo.js
+++ Week3/Day4/nonBlockingDemo.js
@@ -14,5 +14,7 @@ function third() {
 }
 
 first();
-setTimeout(second,2000);
+setTimeout(second, 2000);
+//함수의 매개변수로 변수 또는 값을 전달하는 것이 아니라, 함수를 전달하는 것!
+// 콜백함수
 third();
