4f06d126098b1ced93c03f1a96fd339bcdfde737
Ss0Mae <ssomae@protonmail.ch>
2024-08-29 18:07:07 +0900

    Template String

--- Week3/Day4/scope-demo.js
+++ Week3/Day4/scope-demo.js
@@ -2,7 +2,12 @@ if (true) {
     var num1 = 7;
     const num2 = 3; //블록 스코프여서 중괄호 밖에서는 사용 X
     let num3 = 5; // 블록 스코프여서 중괄호 밖에서 사용 X
+
+    //num2 = 10;
+    num3 = 21;
+    //console.log(num1 + "X" + num2 + "=" + num3);
+    console.log(`${num1} X ${num2} = ${num3}`);
 }
 console.log(num1);
-console.log(num2);
-console.log(num3);
+// console.log(num2);
+// console.log(num3);
