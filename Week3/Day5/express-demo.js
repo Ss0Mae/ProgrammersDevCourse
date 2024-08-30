0eb4119b71067cd38a55e534983037ca99eb7064
Ss0Mae <ssomae@protonmail.ch>
2024-08-30 19:55:59 +0900

    API : GET

--- Week3/Day5/express-demo.js
+++ Week3/Day5/express-demo.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
 
+// Get Method + "/"
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+// API : GET + "http://localhost:1234/test"
+// TEST SUCCESS
+app.get('/test', function (req, res) {
+  res.send('TEST SUCCESS')
+})
+
+// API : GET + "http://localhost:1234/test/1"
+// ONE
+app.get('/test/1', function (req, res) {
+  res.send('ONE!')
+})
+
+
+app.listen(1234)
