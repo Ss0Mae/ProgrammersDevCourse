155c8de08a2a972178d11d2ba182b393308557f6
Ss0Mae <ssomae@protonmail.ch>
2024-09-02 18:40:24 +0900

    Mapping

--- Week4/Day1/map-demo.js
+++ Week4/Day1/map-demo.js
@@ -1,3 +1,32 @@
+const express = require('express')
+const app = express()
+
+// 서버 셋팅: 포트 넘버 1234로 셋팅
+app.listen(1234)
+
+app.get('/', function (req, res) {
+    res.send("Hello World");
+})
+
+//localhost:1234/1 => Notebook
+//localhost:1234/2 => Cup
+//localhost:1234/3 => Chair
+
+app.get('/:id', function (req, res) {
+    const { id } = req.params
+    
+    if (db.get(id) === undefined) {
+        console.log("undefined Test");
+        res.json({
+            message: "없는 상품입니다."
+        })
+    } else {
+        res.json({
+            id: id,
+            productName: db.get(parseInt(id))
+        })
+    }
+})
 let db = new Map();
 db.set(1, "NoteBook"); // key- value
 db.set(2, "Cup");
