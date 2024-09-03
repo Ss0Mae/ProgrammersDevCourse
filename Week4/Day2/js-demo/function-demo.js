54bd8247d812cdfbdad0bc4c8f6df1272df71efb
Ss0Mae <ssomae@protonmail.ch>
2024-09-03 17:56:30 +0900

    Function

---
+++ Week4/Day2/js-demo/function-demo.js
@@ -0,0 +1,15 @@
+
+function add1(x, y) {
+    return x + y;
+}
+
+let add2 = function (x, y) {
+    return x + y;
+}
+
+const add3 = (x, y) => { //arrow Function 
+    return x + y;
+}
+console.log(add1(1, 2));
+console.log(add2(1, 2));
+console.log(add3(1, 2));
