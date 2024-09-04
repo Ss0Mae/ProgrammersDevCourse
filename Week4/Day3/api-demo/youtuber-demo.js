7b941438ef29df0473514726b1b317b89de667ed
Ss0Mae <ssomae@protonmail.ch>
2024-09-04 18:29:09 +0900

    POST youtubers

--- Week4/Day3/api-demo/youtuber-demo.js
+++ Week4/Day3/api-demo/youtuber-demo.js
@@ -22,19 +22,42 @@ let youtuber3 = {
 }
 
 let db = new Map();
-db.set(1, youtuber1);
-db.set(2, youtuber2);
-db.set(3, youtuber3);
+let id = 1
+db.set(id++, youtuber1);
+db.set(id++, youtuber2);
+db.set(id++, youtuber3);
 
 app.get('/youtuber/:id', function (req, res) {
     let { id } = req.params;
     id = parseInt(id);
     //console.log(id);
-    if (db.get(id) === undefined) {
+    const youtuber = db.get(id);
+    if (youtuber == undefined) {
         res.json({
             message: "Wrong Access"
         })
     } else {
-        res.json(db.get(id));
+        res.json(youtuber);
     }
+})
+
+app.get('/youtubers', function (req, res) {
+    let start = 0;
+    res.json({
+        for(let i = 0; )
+    })
+})
+
+app.use(express.json()); //req로 날아오는 body 값을 json으로 읽자
+// http 외 무듈인 '미들웨어' : json 설정
+
+app.post('/youtuber', function (req, res) {
+    //body에 숨겨져서 들어온 데이터를 화면에 출력해볼까
+    console.log(req.body); //
+
+    //등록..? Map(db)에 저장(set)을 해야한다.
+    db.set(id++, req.body);
+    res.json({
+        message: `${req.body.channelTitle}님, 유튜버 생활을 응원합니다`
+   });
 })
