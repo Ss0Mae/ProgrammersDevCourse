410897548b6a348a141e0249a4eed712e2a82c6c
Ss0Mae <ssomae@protonmail.ch>
2024-09-04 17:08:57 +0900

    POST_PRACTICE

--- Week4/Day3/app.js
+++ Week4/Day3/app.js
@@ -6,8 +6,12 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
+app.use(express.json()); //req로 날아오는 body 값을 json으로 읽자
 app.post('/test', function (req, res) {
-    res.send("Hello Post");
+    //body에 숨겨져서 들어온 데이터를 화면에 출력해볼까
+
+    console.log(req.body.message); //undefined
+    res.send(req.body.message);
 })
 
 app.listen(port, () => {
