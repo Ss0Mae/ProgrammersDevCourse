6cfbb4ba95fdef7d97db445bcacf73976b43adc0
Ss0Mae <ssomae@protonmail.ch>
2024-09-06 16:29:31 +0900

    == vs ===

---
+++ Week4/Day5/js-demo/equal-demo.js
@@ -0,0 +1,14 @@
+
+// ==, ===의 차이
+
+if (1 == "1") { //자료형은 상관 없이 값만 비교
+    console.log("same");
+} else {
+    console.log("different");
+}
+
+if (1 === "1") { //자료형과 값과 동시 비교
+    console.log("same");
+} else {
+    console.log("different");
+}
