3cc75ffc8cdf3d675925b08dec9fb9902422c40f
Ss0Mae <ssomae@protonmail.ch>
2024-09-09 18:12:21 +0900

    개별 생성, 개별 조회

--- Week5/Day1/channel-demo.js
+++ Week5/Day1/channel-demo.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 app.listen(7777);
@@ -9,20 +10,41 @@ let id = 1;
 app
     .route('/channels')
     .get((req, res) => {
-        res.send("개별 생성")
-    }) //채널 개별 생성
-    .post((req, res) => {
         res.send("전체 조회")
     }) //채널 전체 조회
+    .post((req, res) => {
+        const { channelTitle } = req.body;
+        if (channelTitle) {
+            db.set(id++, req.body);
+            res.status(201).json({
+                message: `${channelTitle}채널을 응원합니다`
+            });
+        } else {
+            res.status(400).json({
+                message : "채널명이 없습니다"
+            })
+        }
+    }) //채널 개별 생성
 
 app
     .route('/channels/:id')
     .put((req, res) => {
         res.send("개별 수정")
     }) //채널 개별 수정
+
     .delete((req, res) => {
         res.send("개별 삭제")
     }) //채널 개별 삭제
+
     .get((req, res) => {
-        res.send("개별 조회")
+        let { id } = req.params;
+        id = parseInt(id);
+        channelInfo = db.get(id);
+        if (channelInfo) {
+            res.status(200).json(channelInfo);
+        } else {
+            res.status(404).json({
+                message : '채널 정보를 찾을 수 없습니다'
+            })
+        }
     }) //채널 개별 조회
