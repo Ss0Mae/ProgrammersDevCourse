dc570a2a7a30ac579c35bf01b85d08614d2cd025
Ss0Mae <ssomae@protonmail.ch>
2024-09-13 20:18:48 +0900

    Set TimeZone

--- Week5/Day5/db-demo.js
+++ Week5/Day5/db-demo.js
@@ -5,7 +5,8 @@ const connection = mysql.createConnection({
   host: 'localhost',
     user: 'root',
   password : '1234',
-  database: 'Youtube',
+    database: 'Youtube',
+  dateStrings : true
 });
 
 // A simple SELECT query
