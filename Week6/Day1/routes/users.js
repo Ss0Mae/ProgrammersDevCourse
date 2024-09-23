a858f3ee96024f40c7338577b2b2ffc92c0355a2
Ss0Mae <ssomae@protonmail.ch>
2024-09-23 18:31:35 +0900

    code prettier

--- Week6/Day1/routes/users.js
+++ Week6/Day1/routes/users.js
@@ -2,29 +2,18 @@ const express = require('express');
 const router = express.Router();
 const conn = require('../mariadb')
 
-
-
 router.use(express.json()) // http 외 모듈 'json' 사용
 
-let db = new Map();
-let id = 1; //객체 식별 아이디
-
-function isExist(obj) {
-    if (Object.keys(obj).length) return true;
-    else return false;
-}
 // 로그인
 router.post('/login', function (req, res) {
-
-    //id가 db에 저장된 회원인지 확인
     const { email, password } = req.body;
-   
     let loginUser = {};
-    conn.query(
-        `SELECT * FROM users WHERE email = ?`, email,
-        function (err, results, fields) {
+    let sql = `SELECT * FROM users WHERE email = ?`
+    conn.query(sql,email,
+        function (err, results) { 
             loginUser = results[0];
-            if (loginUser && loginUser.password === password) {
+
+            if (loginUser && loginUser.password === password) { //email이 db에 저장된 회원인지 확인
                 res.status(200).json({
                     message: `${loginUser.name}님 로그인 되었습니다.`
                 })
@@ -34,21 +23,19 @@ router.post('/login', function (req, res) {
                     message : "이메일 또는 비밀번호가 틀렸습니다"
                 })
             }
-            
         }
     );  
 })
 
 // 회원 가입
 router.post('/join', function (req, res) {
-    console.log(req.body);
     // 입력된 body 객체가 비어 있지 않은지 확인
     if (Object.keys(req.body).length !== 0) {
         const { email, name, password, contact } = req.body;
+        let sql = `INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)`;
+        let values = [email, name, password, contact]; 
 
-        conn.query(
-            `INSERT INTO users (email, name, password, contact) 
-            VALUES (?, ?, ?, ?)`, [email, name, password, contact],
+        conn.query(sql,values,
             function (err, results, fields) {
                 res.status(201).json(results);
             }
@@ -64,24 +51,20 @@ router
     .route('/users')
     .get(function (req, res) {
         let { email } = req.body;
-        // A simple SELECT query
-        conn.query(
-            `SELECT * FROM users WHERE email = ?`, email,
+        let sql = `SELECT * FROM users WHERE email = ?`
+        conn.query(sql,email,
             function (err, results, fields) {
-                 res.status(200).json(results)
-                
+                 res.status(200).json(results)          
             }
         );   
             
     })
     .delete(function (req, res) {
         let { email } = req.body;
-        // A simple SELECT query
-        conn.query(
-            `DELETE FROM users WHERE email = ?`, email,
+        let sql = `DELETE FROM users WHERE email = ?`
+        conn.query(sql, email,
             function (err, results, fields) {
                  res.status(200).json(results)
-                
             }
         ); 
     })
