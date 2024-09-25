afdcf48d5c3a9add0c59593683de0f1ce70ab87d
Ss0Mae <ssomae@protonmail.ch>
2024-09-25 20:04:02 +0900

    JWT, dotenv

--- Week6/Day3/tempCodeRunnerFile.js
+++ Week6/Day3/tempCodeRunnerFile.js
@@ -1,8 +1,13 @@
-// Using placeholders
-// connection.query(
-//   'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-//   ['Page', 45],
-//   function (err, results) {
-//     console.log(results);
-//   }
-// );
+var jwt = require('jsonwebtoken');
+
+require('dotenv').config({ path: './.env' });
+console.log('Private Key:', process.env.PRIVATE_KEY);
+// var token = jwt.sign({ foo: 'bar' }, process.env.PRIVATE_KEY);
+// //token 생성 = jwt 서명을 했다. (페이로드, 나만의 암호키) + SHA256
+
+// console.log(token);
+
+// // 검증
+// // 만약 검증에 성공하면, 페이로드 값을 확인할 수 있음
+// let decoded = jwt.verify(token, process.env.PRIVATE_KEY);
+// console.log(decoded);
