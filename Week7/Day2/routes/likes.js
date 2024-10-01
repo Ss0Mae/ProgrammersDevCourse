d1070a766de237a537eb1429a3d33f38e2f940be
Ss0Mae <ssomae@protonmail.ch>
2024-10-01 18:35:47 +0900

    likes module

--- Week7/Day2/routes/likes.js
+++ Week7/Day2/routes/likes.js
@@ -4,16 +4,13 @@ const router = express.Router();
 router.use(express.json());
 
 // 좋아요 추가
-router.post('/books', (req, res) => {
-    res.json('전체 도서 조회 ');
+router.post('/likes/:id', (req, res) => {
+    res.json('좋아요 추가');
 });
 
 // 좋아요 삭제
-router.post('/books/:id',(req, res) => {
-    res.json('개별 도서 조회');
+router.delete('/likes/:id',(req, res) => {
+    res.json('좋아요 삭제');
 });
-
-
-
 
 module.exports = router;
