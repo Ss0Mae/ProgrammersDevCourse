562e82ed8b01664a4ff06b7834337b73380792b5
Ss0Mae <ssomae@protonmail.ch>
2024-10-01 18:42:09 +0900

    module Modify

--- Week7/Day2/app.js
+++ Week7/Day2/app.js
@@ -7,8 +7,13 @@ dotenv.config();
 app.listen(process.env.PORT);
 
 const userRouter = require('./routes/users');
+const bookRouter = require('./routes/books');
+const likeRouter = require('./routes/likes');
+const cartRouter = require('./routes/carts');
+const orderRouter = require('./routes/orders');
 
 app.use("/users", userRouter);
-app.use("/users", userRouter);
-app.use("/users", userRouter);
-app.use("/users", userRouter);
+app.use("/books", userRouter);
+app.use("/likes", userRouter);
+app.use("/carts", userRouter);
+app.use("/orders", userRouter);
--- Week7/Day2/routes/carts.js
+++ Week7/Day2/routes/carts.js
@@ -4,17 +4,17 @@ const router = express.Router();
 router.use(express.json());
 
 // 장바구니 담기 
-router.post('/carts', (req, res) => {
+router.post('/', (req, res) => {
     res.json('장바구니 담기');
 });
 
 // 장바구니 조회
-router.get('/carts',(req, res) => {
+router.get('/',(req, res) => {
     res.json('장바구니 조회');
 });
 
 // 장바구니 도서 삭제
-router.get('/carts/:id',(req, res) => {
+router.get('/:id',(req, res) => {
     res.json('장바구니 도서 삭제');
 });
 
