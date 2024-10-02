8c9031527e741f45479c348fa00de8a8a996e9b5
Ss0Mae <ssomae@protonmail.ch>
2024-10-02 17:59:58 +0900

    controller setting

--- Week7/Day3/routes/users.js
+++ Week7/Day3/routes/users.js
@@ -2,27 +2,12 @@ const express = require('express');
 const router = express.Router();
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
+const join = require('../controller/userController');
 
 router.use(express.json());
 
 //회원가입
-router.post('/join', (req, res) => {
-    const { email, password } = req.body;
-    
-    let sql = 'INSERT INTO users (email, password) VALUES(?, ?)';
-    let values = [email, password];
-
-    conn.query(sql, values,
-        (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            res.status(StatusCodes.CREATED).json(results);
-        }
-    )
-    res.json('회원가입');
-});
+router.post('/join',join );
 
 //로그인
 router.post('/login',(req, res) => {
