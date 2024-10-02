8b67788f8a1e5790d558a4bacf425e2a7d09fc0a
Ss0Mae <ssomae@protonmail.ch>
2024-10-02 19:11:05 +0900

    password reset

--- Week7/Day3/controller/userController.js
+++ Week7/Day3/controller/userController.js
@@ -1,5 +1,11 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto'); // crypto 모듈 : 암호화를 담당
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const join = (req, res) => {
     const { email, password } = req.body;
     
@@ -12,9 +18,92 @@ const join = (req, res) => {
                 console.log(err);
                 return res.status(StatusCodes.BAD_REQUEST).end();
             }
-            res.status(StatusCodes.CREATED).json(results);
+            return res.status(StatusCodes.CREATED).json(results);
         }
     )
-}
+};
 
-module.exports = join;
+const login = (req, res) => {
+    const { email, password } = req.body;
+
+    let sql = 'SELECT * FROM users WHERE email = ?';
+    conn.query(sql, email,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            console.log(results, results[0]);
+            const loginUser = results[0];
+            if (loginUser && loginUser.password == password) {
+                //토큰 발행
+                const token = jwt.sign({
+                    email: loginUser.email
+                }, process.env.PRIVATE_KEY, {
+                    expiresIn: '10m',
+                    issuer: 'ssomae'
+                });
+
+                //토큰 쿠키에 담기
+                res.cookie("token", token, {
+                    httpOnly: true
+                });
+                console.log(token);
+
+                return res.status(StatusCodes.OK).json(results);
+            } else {
+                return res.status(StatusCodes.UNAUTHORIZED).end();
+            }
+            
+        }
+    )
+};
+
+const passwordResetRequest = (req, res) => {
+    const { email } = req.body;
+
+    let sql = 'SELECT * FROM users WHERE email = ?';
+    conn.query(sql, email,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            //이메일이 DB에 있는 정보인지 체크
+            const user = results[0];
+            if (user) {
+                return res.status(StatusCodes.OK).json({
+                    email : email
+                });
+            } else {
+                return res.statuts(StatusCodes.UNAUTHORIZED).end();
+            }
+        }
+    )
+};
+
+const passwordReset = (req, res) => {
+    const { email, password } = req.body;
+
+    let sql = 'UPDATE users SET password = ? WHERE email = ?';
+    let values = [password, email];
+    conn.query(sql, values,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            if (results.affectedRows === 0)
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            else
+                return res.status(StatusCodes.OK).json(results);
+        }
+    )
+};
+
+module.exports = {
+    join,
+    login,
+    passwordResetRequest,
+    passwordReset
+};
