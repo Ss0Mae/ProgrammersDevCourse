17c1a36e3232ce451e47a3f9a9336abc8184f141
Ss0Mae <ssomae@protonmail.ch>
2024-10-06 22:50:44 +0900

    카테고리 별 도서 조회

--- Week7/Day4/routes/books.js
+++ Week7/Day4/routes/books.js
@@ -8,8 +8,9 @@ const {
 
 router.use(express.json());
 
+router.get('/',booksByCategory);// 카테고리별 도서 목록 조회
 router.get('/', allBooks); // 전체 도서 조회 
 router.get('/:id',bookDetail);// 개별 도서 조회
-router.get('/',booksByCategory);// 카테고리별 도서 목록 조회
+
 
 module.exports = router;
