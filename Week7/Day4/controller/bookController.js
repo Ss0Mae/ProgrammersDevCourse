3969123c13e2b19360ef448a23bab703fdbe46f4
Ss0Mae <ssomae@protonmail.ch>
2024-10-06 23:02:50 +0900

    카테고리별 도서 조회

--- Week7/Day4/controller/bookController.js
+++ Week7/Day4/controller/bookController.js
@@ -5,14 +5,31 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const allBooks = (req, res) => {
-    const sql = `SELECT * FROM books`;
-    conn.query(sql, (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
-        return res.status(StatusCodes.OK).json(results);
-    })
+    let { category_id } = req.query;
+    if (category_id) {
+        let sql = `SELECT * FROM books WHERE category_id = ?`;
+        conn.query(sql, category_id,
+            (err, results)=> {
+                if (err) {
+                    console.log(err);
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+                if (results.length)
+                    return res.status(StatusCodes.OK).json(results);
+                else
+                    return res.status(StatusCodes.NOT_FOUND).end();
+            }
+        )
+    } else {
+        sql = `SELECT * FROM books`;
+        conn.query(sql, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            return res.status(StatusCodes.OK).json(results);
+        })   
+    }
 };
 
 const bookDetail = (req, res) => {
@@ -32,25 +49,10 @@ const bookDetail = (req, res) => {
     )
 };
 
-const booksByCategory = (req, res) => {
-    let { category_id } = req.query;
-    const sql = `SELECT * FROM books WHERE category_id = ?`;
-    conn.query(sql, category_id,
-        (err, results)=> {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            if (results.length)
-                return res.status(StatusCodes.OK).json(results);
-            else
-                return res.status(StatusCodes.NOT_FOUND).end();
-        }
-    )
-}; 
+
 
 module.exports = {
     allBooks,
     bookDetail,
-    booksByCategory
+    
 };
--- Week7/Day4/routes/books.js
+++ Week7/Day4/routes/books.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const {
     allBooks,
     bookDetail,
-    booksByCategory
+    
 } = require('../controller/bookController');
 
 router.use(express.json());
 
-router.get('/',booksByCategory);// 카테고리별 도서 목록 조회
-router.get('/', allBooks); // 전체 도서 조회 
+
+router.get('/', allBooks); // 전체 도서 조회 및 카테고리 별 도서 조회
 router.get('/:id',bookDetail);// 개별 도서 조회
 
 
