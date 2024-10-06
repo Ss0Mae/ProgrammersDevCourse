91188b639d739e51ad65ad9be39ff37fc3b09e6a
Ss0Mae <ssomae@protonmail.ch>
2024-10-06 22:34:16 +0900

    allBooks, bookDetail

--- Week7/Day4/controller/bookController.js
+++ Week7/Day4/controller/bookController.js
@@ -5,11 +5,31 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const allBooks = (req, res) => {
-    res.json('전체 도서 조회 ');
+    const sql = `SELECT * FROM books`;
+    conn.query(sql, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+        return res.status(StatusCodes.OK).json(results);
+    })
 };
 
 const bookDetail = (req, res) => {
-    res.json('개별 도서 조회');
+    let { id } = req.params;
+    const sql = `SELECT * FROM books WHERE id = ?`;
+    conn.query(sql, id,
+        (err, results)=> {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            if (results[0])
+                return res.status(StatusCodes.OK).json(results[0]);
+            else
+                return res.status(StatusCodes.NOT_FOUND).end();
+        }
+    )
 };
 
 const booksByCategory = (req, res) => {
