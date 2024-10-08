8cf317b8bfa50b1bf5d9eb747ee3bc053013fb3c
Ss0Mae <ssomae@protonmail.ch>
2024-10-08 23:47:47 +0900

    category Table Name Fix

--- Week8/Day2/controller/bookController.js
+++ Week8/Day2/controller/bookController.js
@@ -43,10 +43,17 @@ const allBooks = (req, res) => {
 };
 
 const bookDetail = (req, res) => {
-    let { id } = req.params;
-    let sql = `SELECT * FROM books LEFT JOIN category 
-                    ON books.category_id = category.id WHERE books.id = ?`;
-    conn.query(sql, id,
+    let  book_id = req.params.id;
+    let { user_id } = req.body;
+    let sql = `SELECT *,
+                (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes,
+                (SELECT EXISTS (SELECT * FROM likes WHERE user_id = ? AND liked_book_id =?)) AS liked
+                FROM books
+                LEFT JOIN category
+                ON books.category_id = category.category_id
+                WHERE books.id =?`;
+    let values = [user_id, book_id, book_id];
+    conn.query(sql, values,
         (err, results)=> {
             if (err) {
                 console.log(err);
