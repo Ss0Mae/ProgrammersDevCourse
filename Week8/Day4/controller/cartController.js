e7af68cdbf1e64c50869ef0747836fe4d58cdcad
Ss0Mae <ssomae@protonmail.ch>
2024-10-10 23:36:47 +0900

    deleteCartItems

--- Week8/Day4/controller/cartController.js
+++ Week8/Day4/controller/cartController.js
@@ -7,7 +7,7 @@ dotenv.config();
 const addToCart = (req, res) => {
     const { book_id, quantity, user_id } = req.body;
 
-    let sql = `INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?,?,?)`;
+    let sql = `INSERT INTO cartitems (book_id, quantity, user_id) VALUES (?,?,?)`;
     let values = [book_id, quantity, user_id];
     conn.query(sql, values,
         (err, results) => {
@@ -37,7 +37,19 @@ const getCartItems = (req, res) => {
 };
 
 const removeCartItem = (req, res) => {
-    res.json('장바구니 도서 삭제');
+    const { id } = req.params;
+
+    let sql = `DELETE FROM cartitems WHERE id = ?;`;
+
+    conn.query(sql, id,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            return res.status(StatusCodes.OK).json(results);
+        }
+    )
 };
 
 module.exports = {
