8369f6bd1caa0b85898d0a069f3085acd45ca7b3
Ss0Mae <ssomae@protonmail.ch>
2024-10-14 20:29:03 +0900

    SQL with async

--- Week9/Day1/controller/orderController.js
+++ Week9/Day1/controller/orderController.js
@@ -1,23 +1,23 @@
-const conn = require('../mariadb');
+//const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
-
+const mariadb = require('mysql2/promise');
 //주문 하기
 const order = async (req, res) => {
+    const conn = await mariadb.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: '1234',
+        database: 'Bookshop',
+        dateStrings: true
+    });
     const { items, delivery, totalQuantity, totalPrice,userId, firstBookTitle } = req.body;
     let delivery_id;
     let order_id;
 
     let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
     let values = [delivery.address, delivery.receiver, delivery.contact];
-    let [results] = await conn.query(sql, values,
-        (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            delivery_id = results.insertId;
-        }
-    )
+
+    let [results] = await conn.query(sql, values);
     sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) 
             VALUES (?, ?, ?, ?, ?);`;
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
