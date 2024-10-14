84432003265b67ff9a9b546786bd7eab334e99b3
Ss0Mae <ssomae@protonmail.ch>
2024-10-14 17:15:36 +0900

    Flow

--- Week9/Day1/controller/orderController.js
+++ Week9/Day1/controller/orderController.js
@@ -6,33 +6,30 @@ const order = (req, res) => {
     const { items, delivery, totalQuantity, totalPrice,userId, firstBookTitle } = req.body;
 
     let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
-    let delivery_id = 3;
-    let order_id = 2;
+    let delivery_id;
+    let order_id;
     let values = [delivery.address, delivery.receiver, delivery.contact];
-    // conn.query(sql, values,
-    //     (err, results) => {
-    //         if (err) {
-    //             console.log(err);
-    //             return res.status(StatusCodes.BAD_REQUEST).end();
-    //         }
-    //         delivery_id = results.insertId;
-    //         return res.status(StatusCodes.OK).json(results);
-    //     }
-    // )
+    conn.query(sql, values,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            delivery_id = results.insertId;
+        }
+    )
     sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) 
             VALUES (?, ?, ?, ?, ?);`;
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-    // conn.query(sql, values,
-    //     (err, results) => {
-    //         if (err) {
-    //             console.log(err);
-    //             return res.status(StatusCodes.BAD_REQUEST).end();
-    //         }
-    //         order_id = results.insertId;
-    //         console.log(order_id);
-    //         return res.status(StatusCodes.OK).json(results);
-    //     }
-    // )
+    conn.query(sql, values,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            order_id = results.insertId;
+        }
+    )
 
     sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`;
     values = [];
