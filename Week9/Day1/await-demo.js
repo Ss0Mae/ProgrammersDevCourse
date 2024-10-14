0d111c7759b240ff4471c1cac42846728b47f701
Ss0Mae <ssomae@protonmail.ch>
2024-10-14 18:49:29 +0900

    awaits

--- Week9/Day1/await-demo.js
+++ Week9/Day1/await-demo.js
@@ -5,11 +5,22 @@
 // await을 만나면 Promise 객체를 기다려준다.
 // Promise.then을 함수 밖이 아닌 안에서 작성하게 해준다.
 async function f() {
-    let promise = new Promise((resolve, reject) => {
-        setTimeout(() => resolve('Done'), 3000);
+    let promise1 = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('1st Query'), 3000);
     });
+    let result1 = await promise1;
+    console.log(result1);
 
-    let result = await promise;
-    console.log(result);
+    let promise2 = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('2nd Query with '+ result1), 3000);
+    });
+    let result2 = await promise2;
+    console.log(result2);
+
+    let promise3 = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('3rd Query with '+ result2), 3000);
+    });
+    let result3 = await promise3;
+    console.log(result3);
 }
 f();
