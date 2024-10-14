7f955c6b8df5aea9381cfdedb6778dd3913842de
Ss0Mae <ssomae@protonmail.ch>
2024-10-14 18:58:07 +0900

    mariadb promise

--- Week9/Day1/mariadb.js
+++ Week9/Day1/mariadb.js
@@ -1,13 +1,16 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // Create the connection to database
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password : '1234',
-  database: 'Bookshop',
-  dateStrings : true
-});
+const connection = async () => {
+  const conn = await mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '1234',
+    database: 'Bookshop',
+    dateStrings: true
+  });
+  return conn;
+}
 
 module.exports = connection;
 
