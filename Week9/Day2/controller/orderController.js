2a21cb7d92beec2ad6264b2d19466ad40428758b
Ss0Mae <ssomae@protonmail.ch>
2024-10-15 20:36:10 +0900

    order

--- Week9/Day2/controller/orderController.js
+++ Week9/Day2/controller/orderController.js
@@ -11,40 +11,29 @@ const order = async (req, res) => {
         dateStrings: true
     });
     const { items, delivery, totalQuantity, totalPrice,userId, firstBookTitle } = req.body;
-    let delivery_id;
-    let order_id;
-
+    
+    //delivery 테이블 삽입
     let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
     let values = [delivery.address, delivery.receiver, delivery.contact];
+    let [results] = await conn.execute(sql, values);
+    let delivery_id = results.insertId;
 
-    let [results] = await conn.query(sql, values);
+    // orders 테이블 삽입
     sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) 
             VALUES (?, ?, ?, ?, ?);`;
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-    conn.query(sql, values,
-        (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            order_id = results.insertId;
-        }
-    )
+    [results] = await conn.execute(sql, values);
+    let order_id = results.insertId;
 
+    //orderedBook 테이블 삽입
     sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`;
     values = [];
     items.forEach((item) => {
         values.push([order_id, item.book_id, item.quantity]);
     })
-    conn.query(sql, [values],
-        (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            return res.status(StatusCodes.OK).json(results);
-        }
-    )
+    results = await conn.query(sql, [values]);
+    
+    return res.status(StatusCodes.OK).json(results[0]);
 };
 
 // 주문 목록 조회
