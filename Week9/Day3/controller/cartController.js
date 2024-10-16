517daad8a73288fd604028835af0de9405beca36
Ss0Mae <ssomae@protonmail.ch>
2024-10-16 16:19:05 +0900

    directory setting

---
+++ Week9/Day3/controller/cartController.js
@@ -0,0 +1,61 @@
+const conn = require('../mariadb');
+const { StatusCodes } = require('http-status-codes');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const addToCart = (req, res) => {
+    const { book_id, quantity, user_id } = req.body;
+
+    let sql = `INSERT INTO cartitems (book_id, quantity, user_id) VALUES (?,?,?)`;
+    let values = [book_id, quantity, user_id];
+    conn.query(sql, values,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            return res.status(StatusCodes.OK).json(results);
+        }
+    )
+};
+
+const getCartItems = (req, res) => {
+    const { user_id, selected } = req.body; //selected -> array 
+    let sql = `SELECT cartitems.id, book_id, title, summary, quantity, price 
+                FROM cartitems LEFT JOIN books 
+                ON cartitems.book_id = books.id
+                WHERE user_id = ? AND cartitems.id IN (?)`;
+    let values = [user_id, selected]
+    conn.query(sql, values,
+        (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+        return res.status(StatusCodes.OK).json(results);
+    }) 
+};
+
+const removeCartItem = (req, res) => {
+    const { id } = req.params;
+
+    let sql = `DELETE FROM cartitems WHERE id = ?;`;
+
+    conn.query(sql, id,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            return res.status(StatusCodes.OK).json(results);
+        }
+    )
+};
+
+
+module.exports = {
+    addToCart,
+    getCartItems,
+    removeCartItem
+};
