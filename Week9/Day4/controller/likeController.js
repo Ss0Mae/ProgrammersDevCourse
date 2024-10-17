e1f6f11cc6201930d1186fdbbeb46e21760a21c4
Ss0Mae <ssomae@protonmail.ch>
2024-10-17 20:35:24 +0900

    auth module

--- Week9/Day4/controller/likeController.js
+++ Week9/Day4/controller/likeController.js
@@ -2,22 +2,8 @@ const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
+const ensureAuthorization = require('../auth');
 dotenv.config();
-
-function ensureAuthorization(req,res) {
-    try {
-        let receivedJwt = req.headers["authorization"];
-        console.log("received jwt : ", receivedJwt);
-
-        let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
-        console.log(decodedJwt);
-        return decodedJwt;
-    } catch (err) {
-        console.log(err.name);
-        console.log(err.message);
-        return err;
-    }
-};
 
 const addLike = (req, res) => {
     const book_id  = req.params.id;
