8383ad2437bac8f0e82041534625a9c87f53f4b4
Ss0Mae <ssomae@protonmail.ch>
2024-10-28 18:25:31 +0900

    data type

--- Week11/Day1/app.js
+++ Week11/Day1/app.js
@@ -1,9 +1,11 @@
-var student = {
-    name: 'john',
-    course: 'typescript',
-    score: 100,
-    grade: function () {
-        console.log('A');
-    }
-};
-student.name = 'lee';
+// 변수의 데이터 타입 명시
+var stdId = 1111;
+var stdName = "lee";
+var age = 20;
+var gender = 'male';
+var course = 'Typescript';
+var completed = false;
+// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
+// function Plus(a : number, b : number) : number{
+//     return a + b;
+// }
--- Week11/Day1/app.ts
+++ Week11/Day1/app.ts
@@ -1,10 +1,21 @@
-let student = {
-    name: 'john',
-    course: 'typescript',
-    score: 100,
-    grade: function () {
-        console.log('A');
-    }
+// 변수의 데이터 타입 명시
+let stdId: number = 1111;
+let stdName: string = "lee";
+let age: number = 20;
+let gender: string = 'male';
+let course: string = 'Typescript';
+let completed: boolean = false;
+
+function getInfo(id : number) : {
+    stdId: number;
+    stdName: string;
+    age: number;
+    gender: string;
+    course: string;
+    completed: boolean;
 }
 
-student.name = 'lee';
+// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
+// function Plus(a : number, b : number) : number{
+//     return a + b;
+// }
