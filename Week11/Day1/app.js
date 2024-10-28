b6c853b085d0b2902c07628e5adbef5526d6725e
Ss0Mae <ssomae@protonmail.ch>
2024-10-28 18:59:51 +0900

    method, implements

--- Week11/Day1/app.js
+++ Week11/Day1/app.js
@@ -5,7 +5,47 @@ var age = 20;
 var gender = 'male';
 var course = 'Typescript';
 var completed = false;
-// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
-// function Plus(a : number, b : number) : number{
-//     return a + b;
+var MyStudent = /** @class */ (function () {
+    function MyStudent() {
+        this.stdId = 91011;
+        this.stdName = 'park';
+        this.age = 30;
+        this.gender = 'male';
+        this.course = 'nodejs';
+        this.completed = true;
+    }
+    MyStudent.prototype.setName = function (name) {
+        this.stdName = name;
+        console.log('Set Name: ' + this.stdName);
+    };
+    return MyStudent;
+}());
+var myInstance = new MyStudent();
+myInstance.setName('Choi');
+// function getInfo(id : number) : Student{
+//     return {
+//         stdId: id,
+//         stdName: 'park',
+//         //age: 20,
+//         gender: 'female',
+//         course: 'javascript',
+//         completed: true
+//     };
 // }
+var std = {
+    stdId: 91011,
+    stdName: 'park',
+    age: 30,
+    gender: 'male',
+    course: 'nodejs',
+    completed: true
+};
+function setInfo(student) {
+    console.log(student);
+}
+//setInfo(std)
+//console.log(getInfo(2222));
+// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
+function Plus(a, b) {
+    return a + b;
+}
--- Week11/Day1/app.ts
+++ Week11/Day1/app.ts
@@ -6,16 +6,61 @@ let gender: string = 'male';
 let course: string = 'Typescript';
 let completed: boolean = false;
 
-function getInfo(id : number) : {
+interface Student{
     stdId: number;
-    stdName: string;
-    age: number;
-    gender: string;
-    course: string;
-    completed: boolean;
+    stdName?: string;
+    age?: number;
+    gender?: string;
+    course?: string;
+    completed?: boolean;
+    setName: (name: string) => void;
+    //getName: () => string;
 }
 
-// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
-// function Plus(a : number, b : number) : number{
-//     return a + b;
+class MyStudent implements Student{
+    stdId = 91011;
+    stdName = 'park';
+    age = 30;
+    gender= 'male';
+    course= 'nodejs';
+    completed = true;
+    setName(name: string): void{
+        this.stdName = name;
+        console.log('Set Name: '+this.stdName);
+    }
+}
+
+const myInstance = new MyStudent();
+myInstance.setName('Choi');
+
+// function getInfo(id : number) : Student{
+//     return {
+//         stdId: id,
+//         stdName: 'park',
+//         //age: 20,
+//         gender: 'female',
+//         course: 'javascript',
+//         completed: true
+//     };
 // }
+
+let std = {
+    stdId: 91011,
+    stdName: 'park',
+    age: 30,
+    gender: 'male',
+    course: 'nodejs',
+    completed: true
+};
+
+function setInfo(student : Student) :void {
+    console.log(student);
+}
+//setInfo(std)
+
+//console.log(getInfo(2222));
+
+// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
+function Plus(a : number, b? : number) : number{
+    return a + b;
+}
