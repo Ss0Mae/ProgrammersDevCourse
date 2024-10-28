4fdff6e64f9c4b5d5c0a02c7700b893217408f66
Ss0Mae <ssomae@protonmail.ch>
2024-10-28 17:00:13 +0900

    typescript

---
+++ Week11/Day1/app.js
@@ -0,0 +1,10 @@
+var student = {
+    name: 'john',
+    course: 'typescript',
+    score: 100,
+    grade: function () {
+        console.log('A');
+    }
+};
+student.name = 'lee';
+student.score = 'kim';
---
+++ Week11/Day1/app.ts
@@ -0,0 +1,11 @@
+let student = {
+    name: 'john',
+    course: 'typescript',
+    score: 100,
+    grade: function () {
+        console.log('A');
+    }
+}
+
+student.name = 'lee';
+student.score = 'kim';
