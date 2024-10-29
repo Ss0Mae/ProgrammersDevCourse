2e4e4c64beeb620b7fc594c40cad7333614a12ed
Ss0Mae <ssomae@protonmail.ch>
2024-10-29 19:04:50 +0900

    getter and setter

--- Week11/Day2/oop.js
+++ Week11/Day2/oop.js
@@ -1,14 +1,26 @@
 var Employee = /** @class */ (function () {
-    function Employee(empName, age, empJob) {
+    function Employee(_empName, _age, _empJob) {
         var _this = this;
+        this._empName = _empName;
+        this._age = _age;
+        this._empJob = _empJob;
         this.printEmp = function () {
-            console.log("".concat(_this.empName, "\uC758 \uB098\uC774\uB294 ").concat(_this.age, "\uC774\uACE0 \uC9C1\uC5C5\uC740 ").concat(_this.empJob, "\uC785\uB2C8\uB2E4."));
+            console.log("".concat(_this._empName, "\uC758 \uB098\uC774\uB294 ").concat(_this._age, "\uC774\uACE0 \uC9C1\uC5C5\uC740 ").concat(_this._empJob, "\uC785\uB2C8\uB2E4."));
         };
-        this.empName = empName;
-        this.age = age;
-        this.empJob = empJob;
     }
+    Object.defineProperty(Employee.prototype, "empName", {
+        //getter and setter
+        get: function () {
+            return this._empName;
+        },
+        set: function (val) {
+            this._empName = val;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Employee;
 }());
 var employee1 = new Employee('kim', 24, 'developer');
+employee1.empName = 'lee';
 employee1.printEmp();
--- Week11/Day2/oop.ts
+++ Week11/Day2/oop.ts
@@ -1,17 +1,24 @@
 
 class Employee{
-    empName: string;
-    age: number;
-    empJob: string;
-    constructor(empName : string,age : number,empJob :string) {
-        this.empName = empName;
-        this.age = age;
-        this.empJob = empJob;
+    constructor(
+        private _empName: string,
+        private _age: number,
+        private _empJob: string,
+    ) {
     }
     printEmp = (): void=>{
-        console.log(`${this.empName}의 나이는 ${this.age}이고 직업은 ${this.empJob}입니다.`);
+        console.log(`${this._empName}의 나이는 ${this._age}이고 직업은 ${this._empJob}입니다.`);
+    }
+    //getter and setter
+    get empName() {
+        return this._empName;
+    }
+
+    set empName(val: string) {
+        this._empName = val;
     }
 }
 
 let employee1 = new Employee('kim', 24, 'developer');
+employee1.empName = 'lee';
 employee1.printEmp();
