581d67a6f1d08cd0f2ec2d3390622072df5fadf3
Ss0Mae <ssomae@protonmail.ch>
2024-10-30 17:29:34 +0900

    TSX basic

---
+++ Week11/Day3/todolist/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+      <h1 className = "test">Hello React!!!!!!!</h1>
+      <p>반갑습니다</p>
+    </div>
+  );
+}
+
+// function App() {
+//   return React.createElement("div", null, "Hello React",
+//     React.createElement("p", null, "반갑습니다"));
+//   }
+export default App;
