f8d849d4036f67afa63cfe4650eba8b49d68ad0c
Ss0Mae <ssomae@protonmail.ch>
2024-10-31 16:10:49 +0900

    Class Component

---
+++ Week11/Day4/todolist/src/ClassCom.tsx
@@ -0,0 +1,14 @@
+import { Component, ReactNode } from "react";
+
+// class형 컴포넌트
+class ClassCom extends Component{
+    render() {
+        return (
+            <div>
+                Class Component
+            </div>
+        )
+    }
+}
+
+export default ClassCom;
