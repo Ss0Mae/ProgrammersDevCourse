9d02f641daa522f68775d83b30ff59b4abb19f74
Ss0Mae <ssomae@protonmail.ch>
2024-10-31 20:15:02 +0900

    addTodoList

--- Week11/Day4/todolist/src/App.tsx
+++ Week11/Day4/todolist/src/App.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './Todolist';
-import MapTest from './MapTest';
+
 function App() {
   
   return (
       <div className="container"> 
       <TodoList></TodoList>
-      <MapTest></MapTest>
       </div>
     );
 }
--- Week11/Day4/todolist/src/Todolist.tsx
+++ Week11/Day4/todolist/src/Todolist.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { isTemplateSpan } from "typescript";
+import { Button } from 'react-bootstrap';
 
 /**React.FC = 리액트 Function Component  
  * 리액트에서는 변수대신 state를 자주 사용한다 => 일반 변수와 달리 동적으로 관리하여
@@ -19,16 +21,52 @@ const TodoList : React.FC = () => {
         { id: 3, text: "미팅하기", isChecked : false}
     ]);
 
+    const [newTodo, setNewTodo] = useState <string> ('');
+    const handleCheckedChange = (itemId : number) => {
+        setTodos((prevItems) =>
+            prevItems.map((item) =>
+                item.id === itemId ? { ...item, isChecked: !item.isChecked } : item
+            )
+         )
+    }
+
+    const addTodo = () => {
+        if (newTodo.trim() !== '') {
+            setTodos([...todos, { id: Date.now(), text: newTodo, isChecked: false }]);
+            setNewTodo('');
+        }
+    }
     return (
         <div>
             <h1>{title}</h1>
             <p></p>
             <div className='container'>
+                <div>
+                    <input type='text'
+                        placeholder="할 일 입력"
+                        style={{ marginRight: '10px', writingMode: 'horizontal-tb' }}
+                        onChange= {(e)=>setNewTodo(e.target.value)}
+                    />
+                     <Button variant="primary" onClick={addTodo}>추가</Button>   
+                </div>
+                <p></p>
                 <div className = 'board'>
                     <ul>
                         {
                             todos.map((todo, index) => (
-                                <li key={index}> {todo.text}</li>
+                                <li key={todo.id}>
+                                    <input type='checkbox'
+                                        onChange={() => {
+                                            handleCheckedChange(todo.id);
+                                    }}></input>
+                                    <span>
+                                        {
+                                            todo.isChecked ? 
+                                            <del>{todo.text}</del>
+                                            :<span>{todo.text}</span>
+                                        }
+                                    </span>
+                                </li>
                             ))
                         }
                     </ul>
