e4dafc4d003ac0ac98367fbfcf2cbf5b9af50ae4
Ss0Mae <ssomae@protonmail.ch>
2024-11-01 19:49:33 +0900

    props

--- Week11/Day5/todolist/src/App.tsx
+++ Week11/Day5/todolist/src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import TodoList from './Todolist';
 import Clock from './Timer';
+import MyWeather from './MyWeather';
 function App() {
   
   return (
       <div className="container"> 
       <TodoList></TodoList>
-      <Clock></Clock>
+      {/* <Clock></Clock> */}
+      <MyWeather weather='맑음'>일기예보</MyWeather>
       </div>
     );
 }
