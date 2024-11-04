4317289e8a5277e2d488b2bc155db526bf5d4ddb
Ss0Mae <ssomae@protonmail.ch>
2024-11-04 20:37:41 +0900

    Board Date and Type construct

--- Week12/react-task-app/src/store/slices/boardsSlice.ts
+++ Week12/react-task-app/src/store/slices/boardsSlice.ts
@@ -1,8 +1,52 @@
 import { createSlice } from "../../../node_modules/@reduxjs/toolkit/dist/index";
+import { IBoard } from "../../types/index";
 
-const initialState = {
+type TBoardsState = {
+    modalActive: boolean;
+    boardArray: IBoard[];
+}
+const initialState : TBoardsState = {
     modalActive: false,
-    boardArray : []
+    boardArray: [
+        {
+            boardId: 'board-0',
+            boardName: "첫 번째 게시물",
+            lists: [
+                {
+                    listId: "list-0",
+                    listName: "List 1",
+                    tasks: [
+                        {
+                            taskId: "task-0",
+                            taskName : "Task 1",
+                            taskDescription: "Description",
+                            taskOwner: "ssomae",
+                        },
+                        {
+                            taskId: "task-1",
+                            taskName : "Task 2",
+                            taskDescription: "Description",
+                            taskOwner: "ssomae",
+                        },
+                        
+                    ]
+                },
+                {
+                    listId: "list-1",
+                    listName: "List 2",
+                    tasks: [
+                        {
+                            taskId: "task-3",
+                            taskName : "Task 3",
+                            taskDescription: "Description",
+                            taskOwner: "ssomae",
+                        },
+                        
+                    ]
+                }
+            ]
+        }
+    ]
 }
 
 const boardsSlice = createSlice({
