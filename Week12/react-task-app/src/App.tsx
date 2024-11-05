372d2dadc0bc646339c95eed56aa031e26119aab
Ss0Mae <ssomae@protonmail.ch>
2024-11-05 17:50:09 +0900

    BoardList style

--- Week12/react-task-app/src/App.tsx
+++ Week12/react-task-app/src/App.tsx
@@ -1,26 +1,30 @@
 import { useState } from 'react'
-import { appContainer, buttons } from './App.css'
+import { appContainer, buttons,board } from './App.css'
+import BoardList from './components/BoardList/BoardList'
 
 
 function App() {
-
+  const [activeBoaidId, setActiveBoaidId] = useState('board-0')
   return (
-    <>
-      <div className={appContainer}>
-        <div>
-
-        </div>
-        <div className={buttons}>
-          <button>
-            이 게시판 삭제하기
-          </button>
-          <button>
-
-          </button>
-        </div>
+    
+    <div className={appContainer}>
+        <BoardList activeBoardId={activeBoaidId}
+          setActiveBoardId={setActiveBoaidId}
+        />
+      <div className={board}>
       </div>
+
+      <div className={buttons}>
+        <button>
+          이 게시판 삭제하기
+        </button>
+        <button>
+
+        </button>
+      </div>
+    </div>
       
-    </>
+    
   )
 }
 
--- Week12/react-task-app/src/components/BoardList/BoardList.tsx
+++ Week12/react-task-app/src/components/BoardList/BoardList.tsx
@@ -4,6 +4,8 @@ import { useTypedSelector } from '../../hooks/redux';
 import SideForm from './SideForm/SideForm';
 import { FiPlusCircle } from 'react-icons/fi'
 import { useState } from 'react';
+import { container,title,addSection, addButton, boardItemActive, boardItem } from './BoardList.css';
+import clsx from 'clsx';
 type TBoardListProps = {
   activeBoardId: string;
   setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
@@ -14,24 +16,38 @@ const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId }) => 
   const [isFormOpen, setIsFormOpen] = useState(false);
   const { boardArray } = useTypedSelector(state => state.boards);
   return (
-    <div>
-      <div>
+    <div className={container}>
+      <div className={title}>
         게시판 :
 
       </div>
       {boardArray.map((board, index) => (
-        <div key = {board.boardId}>
+        <div key={board.boardId}
+          onClick = {()=>setActiveBoardId(boardArray[index].boardId)}
+          className={
+            clsx(
+              {
+                [boardItemActive]:
+                boardArray.findIndex(b => b.boardId === activeBoardId) === index,
+              },
+              {
+                [boardItem]:
+                boardArray.findIndex(b=>b.boardId === activeBoardId) !== index
+              }
+            )
+          }
+        >
           <div>
             {board.boardName}
             </div>
         </div>
       ))}
-      <div>
+      <div className={addSection}>
         {
           isFormOpen ?
             <SideForm setIsFormOpen={setIsFormOpen} />
             :
-            <FiPlusCircle onClick={()=>setIsFormOpen(!isFormOpen)}/>
+            <FiPlusCircle className = {addButton} onClick={()=>setIsFormOpen(!isFormOpen)}/>
         }
       </div>
          
