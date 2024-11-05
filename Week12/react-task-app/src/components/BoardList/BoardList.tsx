87ae75e50953809fc034b9202737e51f88e4708a
Ss0Mae <ssomae@protonmail.ch>
2024-11-05 17:29:13 +0900

    BoardList make

--- Week12/react-task-app/src/components/BoardList/BoardList.tsx
+++ Week12/react-task-app/src/components/BoardList/BoardList.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { FC } from 'react'
+import { useTypedSelector } from '../../hooks/redux';
+import SideForm from './SideForm/SideForm';
+import { FiPlusCircle } from 'react-icons/fi'
+import { useState } from 'react';
+type TBoardListProps = {
+  activeBoardId: string;
+  setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
+}
 
-const BoardList = () => {
+
+const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId }) => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const { boardArray } = useTypedSelector(state => state.boards);
   return (
     <div>
-      
+      <div>
+        게시판 :
+
+      </div>
+      {boardArray.map((board, index) => (
+        <div key = {board.boardId}>
+          <div>
+            {board.boardName}
+            </div>
+        </div>
+      ))}
+      <div>
+        {
+          isFormOpen ?
+            <SideForm setIsFormOpen={setIsFormOpen} />
+            :
+            <FiPlusCircle onClick={()=>setIsFormOpen(!isFormOpen)}/>
+        }
+      </div>
+         
     </div>
   )
 }
