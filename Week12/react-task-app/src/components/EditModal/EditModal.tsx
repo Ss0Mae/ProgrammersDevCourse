769163725cadbcdc18c796586f6e5c6437c539a6
Ss0Mae <ssomae@protonmail.ch>
2024-11-06 17:45:19 +0900

    ModalEdit function add

--- Week12/react-task-app/src/components/EditModal/EditModal.tsx
+++ Week12/react-task-app/src/components/EditModal/EditModal.tsx
@@ -1,16 +1,91 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { FiX } from 'react-icons/fi'
+import { v4 } from 'uuid';
 import { useTypedDispatch, useTypedSelector } from '../../hooks/redux'
-import { setModalActive } from '../../store/slices/boardsSlice';
+import { deleteTask, setModalActive, updateTask } from '../../store/slices/boardsSlice';
+import { addLog } from '../../store/slices/loggerSlice';
 
 const EditModal = () => {
 
   const editingState = useTypedSelector(state => state.modal);
   const [data, setData] = useState(editingState);
   const dispatch = useTypedDispatch();
+
   const handleCloseButton = () => {
     dispatch(setModalActive(false));
   }
+  
+
+  const handleNameChange = (e:ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      task: {
+        ...data.task,
+        taskName : e.target.value
+      }
+    })
+  }
+
+  const handleDescriptionChange = (e:ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      task: {
+        ...data.task,
+        taskDescription : e.target.value
+      }
+    })
+  }
+  
+  const handleAuthorChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      task: {
+        ...data.task,
+        taskOwner: e.target.value
+      }
+    })
+  }
+
+  const handleUpdate = () => {
+    dispatch(
+      updateTask({
+        boardId: editingState.boardId,
+        listId: editingState.listId,
+        task : data.task
+      })
+    )
+
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `일 수정하기 ${editingState.task.taskName}`,
+        logAuthor: "User",
+        logTimestamp: String(Date.now())
+      })
+    )
+    dispatch(setModalActive(false));
+  }
+
+  const handleDelete = () => {
+    dispatch(
+      deleteTask({
+        boardId: editingState.boardId,
+        listId: editingState.listId,
+        taskId : editingState.task.taskId
+      })
+    )
+
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `일 수정하기 ${editingState.task.taskName}`,
+        logAuthor: "User",
+        logTimestamp: String(Date.now())
+      })
+    )
+    dispatch(setModalActive(false));
+  }
+
   return (
     <div>
       <div>
@@ -21,23 +96,26 @@ const EditModal = () => {
         <div>제목</div>
         <input
           type='text'
-          value = {data.task.taskName}
+          value={data.task.taskName}
+          onChange={handleNameChange}
         />
         <div>설명</div>
         <input
           type='text'
-           value = {data.task.taskDescription}
+          value={data.task.taskDescription}
+          onChange={handleDescriptionChange}
         />
         <div>생성한 사람</div>
         <input
           type='text'
-          value = {data.task.taskOwner}
+          value={data.task.taskOwner}
+          onChange={handleAuthorChange}
         />
         <div>
-          <button>
+          <button onClick = {handleUpdate}>
             일 수정하기
           </button>
-          <button>
+          <button onClick = {handleDelete}>
             일 삭제하기
           </button>
         </div>
