c7244005c86785319e36d765523397104c6acd24
Ss0Mae <ssomae@protonmail.ch>
2024-11-11 22:30:49 +0900

    Theme Switcher useContext

--- Week13/book-store/src/components/header/ThemeSwitcher.tsx
+++ Week13/book-store/src/components/header/ThemeSwitcher.tsx
@@ -1,13 +1,9 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../context/themeContext";
 import { ThemeName } from "../../style/theme";
 
-interface Props{
-    themeName: ThemeName;
-    setThemeName: (themeName: ThemeName) => void;
-}
-function ThemeSwitcher({ themeName, setThemeName }: Props) {
-    const toggleTheme = () => {
-        setThemeName(themeName === "light" ? "dark" : "light");
-    };
+function ThemeSwitcher() {
+    const { themeName, toggleTheme } = useContext(ThemeContext);
 
     return (
         <button onClick={toggleTheme}>{themeName}</button>
