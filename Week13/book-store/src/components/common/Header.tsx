071de9a61b1e7fabc4b4891fce4d799d3e85854f
Ss0Mae <ssomae@protonmail.ch>
2024-11-13 19:08:02 +0900

    category api

--- Week13/book-store/src/components/common/Header.tsx
+++ Week13/book-store/src/components/common/Header.tsx
@@ -3,25 +3,13 @@ import ThemeSwitcher from "../header/ThemeSwitcher";
 import logo from '../../assets/logo.png';
 import {FaSignInAlt, FaRegUser} from 'react-icons/fa';
 import { Link } from "react-router-dom";
-const CATEGORY = [
-    {
-        id: null,
-        name: "전체",
-    },
-    {
-        id: 0,
-        name: "동화",
-    },
-    {
-        id: 1,
-        name: "소설",
-    },
-    {
-        id: 2,
-        name: "사회",
-    },
-];
+import { useEffect, useState } from "react";
+import { Category } from "../../model/category.model";
+import { fetchCategory } from "../../api/category.api";
+import { useCategory } from "../../hooks/useCategory";
+
 function Header() {
+    const { category } = useCategory();
     return (
         <HeaderStyle>
             <h1 className="logo">
@@ -32,7 +20,7 @@ function Header() {
             <nav className = "category">
                 <ul>
                     {
-                        CATEGORY.map((category) => (
+                        category.map((category) => (
                             <li key={category.id}>
                                 <Link to={category.id === null ? "/books" :
                                     `/books?category_id=${category.id}`}>
