17fb1fb7110e1dbe670b4495c5cb374a425eaccb
Ss0Mae <ssomae@protonmail.ch>
2024-11-13 19:07:31 +0900

    http, category.api

---
+++ Week13/book-store/src/api/category.api.ts
@@ -0,0 +1,8 @@
+import { Category } from "../model/category.model";
+import { httpClient } from "./http";
+
+export const fetchCategory = async () => {
+    const response = await httpClient.get<Category[]>("/category");
+    return response.data;
+
+};
