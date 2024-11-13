aacc02c8b70c482d154864f564c5e2242e809a60
Ss0Mae <ssomae@protonmail.ch>
2024-11-13 18:42:41 +0900

    order model

---
+++ Week13/book-store/src/model/order.model.ts
@@ -0,0 +1,10 @@
+export interface Order{
+    id: number;
+    creadtedAt: string;
+    address: string;
+    receiver: string;
+    contact: string;
+    bookTitle: string;
+    totalQuantity: number;
+    totalPrice: number;
+}
