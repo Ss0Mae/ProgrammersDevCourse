eb842ec290e18f38863feb86682a8067b0fcfd49
Ss0Mae <ssomae@protonmail.ch>
2024-11-14 19:23:49 +0900

    BookItem render

---
+++ Week13/book-store/src/components/books/BookItem.spec.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import BookItem from "./BookItem";
+import { BookStoreThemeProvider } from "../../context/themeContext";
+
+const dummyBook: Book = {
+    id: 1,
+    title: 'Book Title',
+    img: 5,
+    category_id: 1,
+    summary: 'Book Summary',
+    author: 'Book Author',
+    price: 10000,
+    likes: 1,
+    form: "paperback",
+    isbn: "Dummy ISBN",
+    detail: 'Book Detail',
+    pages: 100,
+    contents: 'Book Contents',
+    pubDate: "2021-01-01"
+};
+
+describe("BookItem", () => {
+    it ("렌더 여부", () => {
+        const { getByText } = render(
+            <BookStoreThemeProvider>
+                <BookItem book={dummyBook} />
+            </BookStoreThemeProvider>
+        );
+        expect(getByText(dummyBook.title)).toBeInTheDocument();
+        expect(getByText(dummyBook.summary)).toBeInTheDocument();
+        expect(getByText(dummyBook.author)).toBeInTheDocument();
+        //expect(getByText(dummyBook.price)).toBeInTheDocument();
+        
+    });
+
+});
