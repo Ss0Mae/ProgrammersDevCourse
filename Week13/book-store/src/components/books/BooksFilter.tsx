c38a00554b1b40883a9705c6120ddf86920d23a0
Ss0Mae <ssomae@protonmail.ch>
2024-11-14 23:22:59 +0900

    BooksList, BooksFilter

--- Week13/book-store/src/components/books/BooksFilter.tsx
+++ Week13/book-store/src/components/books/BooksFilter.tsx
@@ -1,13 +1,66 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { styled } from "styled-components";
+import { QUERYSTRING } from "../../constants/querystring";
+import { useCategory } from "../../hooks/useCategory";
+import Button from "../common/Button";
 
 function BooksFilter() {
+
+    const { category } = useCategory();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const handleCategory = (id: number | null) => {
+        const newSearchParams = new URLSearchParams(searchParams);
+        if (id === null) {
+            newSearchParams.delete(QUERYSTRING.CATEGORY_ID);
+        } else {
+            newSearchParams.set(QUERYSTRING.CATEGORY_ID, id.toString());
+        }
+        setSearchParams(newSearchParams);
+    };
+    
+    const handleNews = () => {
+        const newSearchParams = new URLSearchParams(searchParams);
+        if (newSearchParams.get(QUERYSTRING.NEWS)) {
+            newSearchParams.delete(QUERYSTRING.NEWS);
+        } else {
+            newSearchParams.set(QUERYSTRING.NEWS, 'true');
+        }
+         setSearchParams(newSearchParams);
+    };
+
     return (
         <BooksFilterStyle>
-            <h1>BooksFilter</h1>
+            <div className="category">
+                {
+                    category.map((item) => (
+                        <Button size='medium' scheme={item.isActive ? 'primary' : 'normal'}
+                            key={item.id}
+                        onClick = {()=>handleCategory(item.id)}>
+                            {item.name}
+                        </Button>
+                    ))
+                }
+            </div>
+            <div className="new">
+                <Button size='medium' scheme={searchParams.get(QUERYSTRING.NEWS) ? 'primary' : 'normal'}
+                    onClick={() => handleNews()}>
+                    신간
+                </Button>
+            </div>
         </BooksFilterStyle>
     )
 }
 
-const BooksFilterStyle = styled.div``;
+const BooksFilterStyle = styled.div`
+    display: flex;
+    gap : 24px;
+
+    .category{
+        display: flex;  
+        gap: 8px;
+    }
+`;
 
 export default BooksFilter;
--- Week13/book-store/src/components/books/BooksList.tsx
+++ Week13/book-store/src/components/books/BooksList.tsx
@@ -2,30 +2,24 @@ import { styled } from "styled-components";
 import { Book } from "../../model/book.model";
 import BookItem from "./BookItem";
 
-const dummyBook: Book = {
-    id: 1,
-    title: 'Book Title',
-    img: 5,
-    category_id: 1,
-    summary: 'Book Summary',
-    author: 'Book Author',
-    price: 10000,
-    likes: 1,
-    form: "paperback",
-    isbn: "Dummy ISBN",
-    detail: 'Book Detail',
-    pages: 100,
-    contents: 'Book Contents',
-    pubDate : "2021-01-01"
+interface Props {
+    books : Book[];
 }
-function BooksList() {
+function BooksList({books} : Props) {
     return (
         <BooksListStyle>
-            <BookItem book={dummyBook} />
+            {books.map(book => (
+                <BookItem key={book.id} book={book} />
+            ))}
         </BooksListStyle>
     )
 }
 
-const BooksListStyle = styled.div``;
+const BooksListStyle = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4,1fr);
+    gap : 24px;
+    
+`;
 
 export default BooksList;
