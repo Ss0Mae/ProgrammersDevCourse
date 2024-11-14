af3615b479131dcff3842b99a79817a64ac93641
Ss0Mae <ssomae@protonmail.ch>
2024-11-14 23:23:50 +0900

    hooks update

--- Week13/book-store/src/hooks/useCategory.ts
+++ Week13/book-store/src/hooks/useCategory.ts
@@ -1,9 +1,34 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { fetchCategory } from "../api/category.api";
 import { Category } from "../model/category.model";
 
 export const useCategory = () => {
     const [category, setCategory] = useState<Category[]>([]);
+    const location = useLocation();
+
+    const setActive = () => {
+        const params = new URLSearchParams(location.search);
+        if (params.get('category_id')) {
+            setCategory((prev) => {
+                return prev.map((item) => {
+                    return {
+                        ...item,
+                        isActive: item.id === Number(params.get('category_id'))
+                    }
+                })
+            })
+        } else {
+            setCategory((prev) => {
+                return prev.map((item) => {
+                    return {
+                        ...item,
+                        isActive: false
+                    }
+                })
+            })
+        }
+    }
     useEffect(() => {
         fetchCategory().then((category) => {
             if(!category) return;
@@ -15,7 +40,13 @@ export const useCategory = () => {
                 ...category
             ]
             setCategory(categoryWithAll);
+            setActive();
         });
     }, [])
+
+    useEffect(() => {
+        setActive();
+     }, [location.search])
+
     return { category };
 }
