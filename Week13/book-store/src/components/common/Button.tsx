d4a36a5779ee408265b02393f3a66fc97072d43f
Ss0Mae <ssomae@protonmail.ch>
2024-11-15 22:28:02 +0900

    button fix

--- Week13/book-store/src/components/common/Button.tsx
+++ Week13/book-store/src/components/common/Button.tsx
@@ -4,7 +4,7 @@ import { ButtonScheme, ButtonSize } from '../../style/theme';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement>{
     children: React.ReactNode;
-    size: ButtonSize
+    size: ButtonSize;
     scheme: ButtonScheme;
     disabled?: boolean;
     isLoading? : boolean;
@@ -23,9 +23,9 @@ const ButtonStyle = styled.button<Omit<Props, "children">>`
     background-color: ${({ theme, scheme }) => theme.
     buttonScheme[scheme].backgroundColor};
     border: 0;
-    border-radius : ${({ theme }) => theme.borderRadius.defualt};
-    opacity: ${({ disabled }) => disabled ? 0.5 : 1};
-    pointer-events: ${({ disabled }) => disabled ? "none" : "auto"};
-    cursor: ${({ disabled }) => disabled ? "not-allowed" : "pointer"};
+    border-radius : ${({ theme }) => theme.borderRadius.default};
+    opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
+    pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")};
+    cursor: ${({ disabled }) => (disabled ? "none" : "pointer")};
 `;
 export default Button
