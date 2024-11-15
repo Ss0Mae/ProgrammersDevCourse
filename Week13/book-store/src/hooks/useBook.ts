de25f6a3fc12e1ee6282c566e018c6f9a788c0a6
Ss0Mae <ssomae@protonmail.ch>
2024-11-15 23:00:19 +0900

    book hook

---
+++ Week13/book-store/src/hooks/useBook.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { fetchBook } from "../api/books.api";
+import { BookDetail } from "../model/book.model";
+
+export const useBook = (bookId: string | undefined) => {
+    const [book, setBook] = useState<BookDetail | null>(null);  
+    
+    useEffect(() => {
+        if (!bookId) return;
+        fetchBook(bookId).then((book) => {
+            setBook(book);
+        });
+    }, [bookId]);
+    return { book };
+};
