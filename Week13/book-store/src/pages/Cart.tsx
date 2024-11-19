4068dd7a1818e6b19c1eec02cf0dcaafc8e9dbd4
Ss0Mae <ssomae@protonmail.ch>
2024-11-19 20:47:14 +0900

    cart page

--- Week13/book-store/src/pages/Cart.tsx
+++ Week13/book-store/src/pages/Cart.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled } from "styled-components";
 import CartItem from "../components/cart/CartItem";
 import Title from "../components/common/Title";
 import { useCart } from "../hooks/useCart";
 
 const Cart = () => {
-  const { carts } = useCart();
+  const { carts, deleteCartItem } = useCart();
+  const [checkedItems, setCheckedItems] = useState<number[]>([]);
+  const handleCheck = (id: number) => {
+    if (checkedItems.includes(id)) {
+      setCheckedItems(checkedItems.filter((item) => item !== id));
+    } else {
+      setCheckedItems([...checkedItems, id]);
+    }
+  };
+
+  const handleItemDelete = (id: number) => {
+      deleteCartItem(id);
+  };
+
   return (
     <>
       <Title size="large">장바구니</Title>
       <CartStyle>
         <div className="content">
           {carts.map((cart) => (
-            <CartItem key={cart.id} cart={cart} />
+            <CartItem
+              key={cart.id}
+              cart={cart}
+              checkedItems={checkedItems}
+              onCheck={handleCheck}
+              onDelete={handleItemDelete}
+            />
           ))}
         </div>
         <div className="summary"></div>
