4c7b0694d245929685ca72a91144923e229bfaf4
Ss0Mae <ssomae@protonmail.ch>
2024-11-19 20:47:49 +0900

    hooks update

--- Week13/book-store/src/hooks/useCart.ts
+++ Week13/book-store/src/hooks/useCart.ts
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
-import { fetchCart } from "../api/carts.api";
+import { deleteCart, fetchCart } from "../api/carts.api";
 import { Cart } from "../model/cart.model";
 
 export const useCart = () => {
     const [carts, setCarts] = useState<Cart[]>([]);
     const [isEmpty, setIsEmpty] = useState<boolean>(true);
+    const deleteCartItem = (id: number) => {
+        deleteCart(id).then(() => {
+            setCarts(carts.filter((cart) => cart.id !== id));
+        })
+    };
     useEffect(() => {
         fetchCart().then((carts) => {
             setCarts(carts);
             setIsEmpty(carts.length === 0);
         });
     },[])
-    return { carts, isEmpty };
+    return { carts, isEmpty, deleteCartItem };
 }
