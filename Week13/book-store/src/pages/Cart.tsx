eec7e1f500647ebb16bac5c2dc919f97a09d780f
Ss0Mae <ssomae@protonmail.ch>
2024-11-19 21:03:11 +0900

    Empty

--- Week13/book-store/src/pages/Cart.tsx
+++ Week13/book-store/src/pages/Cart.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
+import { FaShoppingCart } from "react-icons/fa";
 import { styled } from "styled-components";
 import CartItem from "../components/cart/CartItem";
+import Empty from "../components/common/Empty";
 import Title from "../components/common/Title";
 import { useCart } from "../hooks/useCart";
 
 const Cart = () => {
-  const { carts, deleteCartItem } = useCart();
+  const { carts, deleteCartItem, isEmpty } = useCart();
   const [checkedItems, setCheckedItems] = useState<number[]>([]);
   const handleCheck = (id: number) => {
     if (checkedItems.includes(id)) {
@@ -16,25 +18,36 @@ const Cart = () => {
   };
 
   const handleItemDelete = (id: number) => {
-      deleteCartItem(id);
+    deleteCartItem(id);
   };
 
   return (
     <>
       <Title size="large">장바구니</Title>
       <CartStyle>
-        <div className="content">
-          {carts.map((cart) => (
-            <CartItem
-              key={cart.id}
-              cart={cart}
-              checkedItems={checkedItems}
-              onCheck={handleCheck}
-              onDelete={handleItemDelete}
-            />
-          ))}
-        </div>
-        <div className="summary"></div>
+        {!isEmpty && (
+          <>
+            <div className="content">
+              {carts.map((cart) => (
+                <CartItem
+                  key={cart.id}
+                  cart={cart}
+                  checkedItems={checkedItems}
+                  onCheck={handleCheck}
+                  onDelete={handleItemDelete}
+                />
+              ))}
+            </div>
+            <div className="summary">summary</div>
+          </>
+        )}
+        {isEmpty && (
+          <Empty
+            title="장바구니가 비었습니다"
+            icon={<FaShoppingCart />}
+            description={<p>장바구니에 도서를 담아보세요.</p>}
+          />
+        )}
       </CartStyle>
     </>
   );
